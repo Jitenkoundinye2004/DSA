b7835be3d8a5cf2a15fdb3dce385b1023a4d6a49
Jiten Koundinye <jitenkoundinyecse@gmail.com>
2026-07-24 15:04:43 +0530

    Time: 204 ms (5.09%) | Memory: 47 MB (24.52%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,40 +1,36 @@
 class Solution {
 
-    public int find(ArrayList<Integer>freq){
-        int maxcnt = -1;
-        for(int i =0; i<256;i++){
-            maxcnt=Math.max(maxcnt,freq.get(i));
+    public int find(ArrayList<Integer> freq){
+        int max = -1;
+        for(int i =0 ;i <256; i++){
+            max=Math.max(max,freq.get(i));
         }
-        return maxcnt;
+        return max;
     }
-    
     public int characterReplacement(String s, int k) {
-        int low =0;
-        int n = s.length();
-        int res = Integer.MIN_VALUE;
-        ArrayList<Integer> f = new ArrayList<>();
-
-        for(int i = 0; i<256;i++){
-            f.add(0);
-        }
-        for(int high = 0; high<n;high++){
-            f.set(s.charAt(high)-'A',f.get(s.charAt(high)-'A')+1);
-            int len = high-low+1;
-            int max = find(f);
-            int diff = len-max;
-
-            while(diff>k){
-                f.set(s.charAt(low)-'A',f.get(s.charAt(low)-'A')-1);
-                low++;
-                len = high-low+1;
-                max = find(f);
-                diff = len-max;
-            }
-
+       int n = s.length();
+       int low = 0;
+       int result = Integer.MIN_VALUE;
+       ArrayList<Integer> f = new ArrayList<>();
+       for(int i  =0;i<256; i++){
+        f.add(0);
+       }
+       for(int high = 0;high<n; high++){
+        f.set(s.charAt(high)-'A',f.get(s.charAt(high)-'A')+1);
+        int len = high-low+1;
+        int max = find(f);
+        int diff = len-max;
+        while(diff>k){
+            f.set(s.charAt(low)-'A', f.get(s.charAt(low)-'A') - 1);
+            low++;
             len = high-low+1;
-            res = Math.max(res,len);
+            max = find(f);
+            diff = len-max;
         }
-        return res;
-    
+
+        len = high-low+1;
+        result = Math.max(result,len);
+       }
+       return result;
     }
 }
