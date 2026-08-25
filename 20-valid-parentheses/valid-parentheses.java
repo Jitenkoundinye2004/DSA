b015ea803c2c2b4358e7eb626f71f5140e83c79e
Jiten Koundinye <jitenkoundinyecse@gmail.com>
2026-08-25 16:19:40 +0530

    Time: 5 ms (13.11%) | Memory: 43.8 MB (5.63%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n  = s.length();
+        Stack<Character> st = new Stack<>(); 
+        for(int i =0;i<n;i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+                continue;
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(s.charAt(i)==')' && st.peek()=='('){
+                    st.pop();
+                }else if(s.charAt(i)=='}' && st.peek()=='{'){
+                    st.pop();
+                }else if(s.charAt(i)==']' && st.peek()=='['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
