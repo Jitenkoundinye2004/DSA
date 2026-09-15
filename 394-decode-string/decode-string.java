635530007c609bb4dcb60232cb0ee50cd18fe80e
Jiten Koundinye <jitenkoundinyecse@gmail.com>
2026-09-15 15:02:37 +0530

    Time: 2 ms (35.26%) | Memory: 42.9 MB (59.04%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> count = new Stack<>();
+        Stack<String> Str = new Stack<>();
+
+        int num = 0;
+        String current = "";
+        for(int ch =0;ch<s.length();ch++){
+            if(Character.isDigit(s.charAt(ch))){
+                num = num*10+(s.charAt(ch) - '0');
+            }else if(s.charAt(ch)=='['){
+                count.push(num);
+                Str.push(current);
+
+                num=0;
+                current="";
+            }else if(s.charAt(ch)!=']'){
+                current += s.charAt(ch);
+            }else{
+                int c = count.pop();
+                String previous = Str.pop();
+
+                StringBuilder temp = new StringBuilder(previous);
+
+                for(int i = 0; i<c; i++){
+                    temp.append(current);
+                }
+
+                current= temp.toString();
+            }
+        } 
+        return current;
+    }
+}
